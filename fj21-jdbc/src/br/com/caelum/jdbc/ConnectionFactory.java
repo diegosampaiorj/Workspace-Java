212d7117c2dc68274a025f87e7865f81866fdafe
Diego <diegosampaiorj@gmail.com>
2014-11-27 14:34:41 -0200

    27/11

--- fj21-jdbc/src/br/com/caelum/jdbc/ConnectionFactory.java
+++ fj21-jdbc/src/br/com/caelum/jdbc/ConnectionFactory.java
@@ -8,8 +8,7 @@ public class ConnectionFactory {
 
 	public Connection getConnection() {
 		 try {
-		 return DriverManager.getConnection(
-		 "jdbc:mysql://localhost/fj21", "root", "ie14ih153");
+		 return DriverManager.getConnection("jdbc:mysql://localhost/fj21", "root", "ie14ih153");
 		 } catch (SQLException e) {
 		 throw new RuntimeException(e);
 		 }
